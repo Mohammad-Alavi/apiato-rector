6e933cddb3aa55c7fcad9f062c0c0b048e013088
Mohammad Alavi <Mohammad.alavi1990@gmail.com>
2025-03-02 21:53:45 +0330

    Update RemoveGroupAttributeRector.php

--- src/Rules/RemoveGroupAttributeRector.php
+++ src/Rules/RemoveGroupAttributeRector.php
@@ -6,18 +6,9 @@ use PhpParser\Node;
 use PhpParser\Node\Stmt\Class_;
 use PHPUnit\Framework\Attributes\Group;
 use Rector\Rector\AbstractRector;
-use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
 
 final class RemoveGroupAttributeRector extends AbstractRector
 {
-    public function getRuleDefinition(): RuleDefinition
-    {
-        return new RuleDefinition(
-            'Remove #[Group] attribute from PHPUnit test classes',
-            [],
-        );
-    }
-
     public function getNodeTypes(): array
     {
         return [Class_::class];
